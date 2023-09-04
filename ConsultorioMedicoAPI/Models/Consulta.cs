186c57a8a275b6f76732119f30f4c15468e9fb7d
Raul <raul.rhf9316@gmail.com>
2023-09-04 15:43:56 -0300

    add id end

--- ConsultorioMedicoAPI/Models/Consulta.cs
+++ ConsultorioMedicoAPI/Models/Consulta.cs
@@ -9,9 +9,9 @@ namespace ConsultorioMedicoAPI.Models
         public string? Descricao { get; set; }
         public string? PrescricaoMedica { get; set; }
         public string? Diagnostico { get; set; }
-        public int MedicoId { get; set; }
+        public int? MedicoId { get; set; }
         public Medico? Medico { get; set; }
-        public int PacienteId { get; set; }
+        public int? PacienteId { get; set; }
         public Paciente? Paciente { get; set; }
     }
 }
--- ConsultorioMedicoAPI/Models/Endereco.cs
+++ ConsultorioMedicoAPI/Models/Endereco.cs
@@ -2,6 +2,7 @@
 {
     public class Endereco
     {
+        public int Id { get; set; }
         public string? CEP { get; set; }
         public string? Pais { get; set; }
         public string? Estado { get; set; }

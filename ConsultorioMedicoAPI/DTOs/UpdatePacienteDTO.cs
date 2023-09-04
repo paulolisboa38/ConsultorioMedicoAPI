25d7bffc2cb112eb4876777193dc78d9a110b185
paulolisboa38 <paulolisboa38@gmail.com>
2023-09-04 19:27:24 -0300

    criação classe UpdatePacienteDTO

---
+++ ConsultorioMedicoAPI/DTOs/UpdatePacienteDTO.cs
@@ -0,0 +1,15 @@
+﻿using ConsultorioMedicoAPI.Models;
+
+namespace ConsultorioMedicoAPI.DTOs
+{
+    public class UpdatePacienteDTO
+    {
+        public string? Nome { get; set; }
+        public DateTime? DataDeNascimento { get; set; }
+        public string? Telefone { get; set; }
+        public Endereco? Endereco { get; set; }
+        public string? Email { get; set; }
+        public string? Genero { get; set; }
+        public string? Alerta { get; set; }
+    }
+}

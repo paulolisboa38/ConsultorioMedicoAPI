fb6f93181060cfe4cd3af80a8660c022b02e804f
Paulo Lisboa <paulolisboa38@gmail.com>
2023-09-04 15:05:56 -0300

    criação classe Consulta

---
+++ ConsultorioMedicoAPI/Models/Consulta.cs
@@ -0,0 +1,15 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ConsultorioMedicoAPI.Models
+{
+    public class Consulta
+    {
+        public int Id { get; set; }
+        public DateTime DataConsulta { get; set; }
+        public string? Descricao { get; set; }
+        public string? PrescricaoMedica { get; set; }
+        public string? Diagnostico { get; set; }
+        public int MedicoId { get; set; }
+        public int PacienteId { get; set; }
+    }
+}

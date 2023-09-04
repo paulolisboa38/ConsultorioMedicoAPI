181b754a9d28353c5a28d4a75021e8824164dcc8
Paulo Lisboa <paulolisboa38@gmail.com>
2023-09-04 15:12:44 -0300

    nova propriedade Email

--- ConsultorioMedicoAPI/Models/Medico.cs
+++ ConsultorioMedicoAPI/Models/Medico.cs
@@ -4,12 +4,13 @@
     {
         public int Id { get; set; }
         public string? Nome { get; set; }
+        public DateTime? DataNascimento { get; set; }
         public string? CRM { get; set; }
         public bool AtivoCRM { get; set; }
         public string? Especialidade { get; set; }
+        public string? Email { get; set; }
         public string? Telefone { get; set; }
         public Endereco? Endereco { get; set; }
-        public DateTime? DataNascimento { get; set; }
         public string? Genero { get; set; }
     }
 }
--- ConsultorioMedicoAPI/Models/Paciente.cs
+++ ConsultorioMedicoAPI/Models/Paciente.cs
@@ -4,10 +4,11 @@
     {
         public int Id { get; set; }
         public string? Nome { get; set; }
-        public DateTime? DataDeNascimento{ get; set; }
+        public DateTime? DataDeNascimento { get; set; }
         public string? CPF { get; set; }
         public string? Telefone { get; set; }
         public Endereco? Endereco { get; set; }
+        public string? Email { get; set; }
         public string? Genero { get; set; }
         public string? Alerta { get; set; }
     }

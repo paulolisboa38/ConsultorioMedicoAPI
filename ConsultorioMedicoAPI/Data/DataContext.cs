9a9440cf3849a0da5e9bc01ed99a4605199aadad
Paulo Lisboa <paulolisboa38@gmail.com>
2023-09-04 15:25:49 -0300

    criação classe DataContext

---
+++ ConsultorioMedicoAPI/Data/DataContext.cs
@@ -0,0 +1,31 @@
+﻿using ConsultorioMedicoAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsultorioMedicoAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Consulta> Consultas { get; set; }
+        public DbSet<Medico> Medicos { get; set; }
+        public DbSet<Paciente> Pacientes { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Consulta>()
+                .HasOne(c => c.Medico)
+                .WithMany()
+                .HasForeignKey(c => c.MedicoId);
+
+            modelBuilder.Entity<Consulta>()
+                .HasOne(c => c.Paciente)
+                .WithMany()
+                .HasForeignKey(c => c.PacienteId);
+        }
+    }
+}

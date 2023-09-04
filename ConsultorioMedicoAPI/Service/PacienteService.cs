31581466eff7ea4e87797a75a17bf4bb1d236bb6
paulolisboa38 <paulolisboa38@gmail.com>
2023-09-04 19:29:43 -0300

    criação classe de serviço PacienteService

---
+++ ConsultorioMedicoAPI/Service/PacienteService.cs
@@ -0,0 +1,34 @@
+﻿using ConsultorioMedicoAPI.DTOs;
+using ConsultorioMedicoAPI.Models;
+using ConsultorioMedicoAPI.Service.Interfaces;
+
+namespace ConsultorioMedicoAPI.Service
+{
+    public class PacienteService : IPacienteService
+    {
+        public Task<Paciente> CreatePacienteAsync(CreatePacienteDTO createPacienteDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Consulta>> GetConsultasPorPacienteIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Paciente>> GetPacientesPorIdadeAsync(DateTime dataNascimento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Paciente> UpdatePacienteAsync(UpdatePacienteDTO updatePacienteDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Paciente> UpdatePacienteEnderecoAsync(int id,UpdatePacienteEnderecoDTO updatePacienteEnderecoDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

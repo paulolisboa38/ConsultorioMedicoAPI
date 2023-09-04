b52f9b54147b18db6a3acbd9bd0c8d9738dc723c
paulolisboa38 <paulolisboa38@gmail.com>
2023-09-04 19:29:07 -0300

    Atualização Interface IPacienteService

--- ConsultorioMedicoAPI/Service/Interfaces/IPaciente.cs
+++ ConsultorioMedicoAPI/Service/Interfaces/IPaciente.cs
@@ -1,6 +1,23 @@
-﻿namespace ConsultorioMedicoAPI.Service.Interfaces
+﻿using ConsultorioMedicoAPI.DTOs;
+using ConsultorioMedicoAPI.Models;
+
+namespace ConsultorioMedicoAPI.Service.Interfaces
 {
-    public interface IPaciente
+    public interface IPacienteService
     {
+        // Obter todas as consultas associadas a um paciente específico
+        Task<IEnumerable<Consulta>> GetConsultasPorPacienteIdAsync(int id);
+
+        // Atualizar informações de um paciente, como o número de telefone
+        Task<Paciente> UpdatePacienteAsync(UpdatePacienteDTO updatePacienteDTO);
+
+        // Obter todos os pacientes com mais de uma certa idade
+        Task<IEnumerable<Paciente>> GetPacientesPorIdadeAsync(DateTime dataNascimento);
+
+        // Criar um novo registro de paciente
+        Task<Paciente> CreatePacienteAsync(CreatePacienteDTO createPacienteDTO);
+
+        // Atualizar o endereço de um paciente específico
+        Task<Paciente> UpdatePacienteEnderecoAsync(int id,UpdatePacienteEnderecoDTO updatePacienteEnderecoDTO);
     }
 }

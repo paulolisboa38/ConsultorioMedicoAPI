059ccf8c71e93a43e848e9bdb41acf4ac21b2deb
paulolisboa38 <paulolisboa38@gmail.com>
2023-09-04 19:27:58 -0300

    criação classe CreatePacienteDTO

---
+++ ConsultorioMedicoAPI/DTOs/CreatePacienteDTO.cs
@@ -0,0 +1,16 @@
+﻿using ConsultorioMedicoAPI.Models;
+
+namespace ConsultorioMedicoAPI.DTOs
+{
+    public class CreatePacienteDTO
+    {
+        public string? Nome { get; set; }
+        public DateTime? DataDeNascimento { get; set; }
+        public string? CPF { get; set; }
+        public string? Telefone { get; set; }
+        public Endereco? Endereco { get; set; }
+        public string? Email { get; set; }
+        public string? Genero { get; set; }
+        public string? Alerta { get; set; }
+    }
+}

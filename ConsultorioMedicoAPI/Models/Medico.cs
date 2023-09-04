b3b1592be9345ecadb0dd7484de3231e7585b4c6
paulolisboa38 <paulolisboa38@gmail.com>
2023-09-04 14:44:19 -0300

    criação classe Medico

---
+++ ConsultorioMedicoAPI/Models/Medico.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsultorioMedicoAPI.Models
+{
+    public class Medico
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string CRM { get; set; }
+        public bool AtivoCRM { get; set; }
+        public string Especialidade { get; set; }
+        public string Telefone { get; set; }
+        public string Endereco { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Genero { get; set; }
+    }
+}

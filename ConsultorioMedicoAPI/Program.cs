5daca2bd11359a14a7f89eca353ff934b346ae6a
Raul <raul.rhf9316@gmail.com>
2023-09-06 11:57:01 -0300

    fix erro de criaçao ciclica

--- ConsultorioMedicoAPI/Program.cs
+++ ConsultorioMedicoAPI/Program.cs
@@ -2,10 +2,17 @@ using ConsultorioMedicoAPI.Data;
 using ConsultorioMedicoAPI.Service;
 using ConsultorioMedicoAPI.Service.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+        options.JsonSerializerOptions.WriteIndented = true;
+    });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

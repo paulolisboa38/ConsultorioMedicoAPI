1d4376727827322e3526fba54d2c11adde6fab80
paulolisboa38 <paulolisboa38@gmail.com>
2023-09-06 05:33:04 -0300

    criação método VerificarAnoNascimento

--- ConsultorioMedicoAPI/Utils/Validadores.cs
+++ ConsultorioMedicoAPI/Utils/Validadores.cs
@@ -1,11 +1,21 @@
 ﻿namespace ConsultorioMedicoAPI.Utils
 {
-    public class Validadores
+    public static class Validadores
     {
         public static bool VerificarZeroNaData(int numero)
         {
             string numeroStr = numero.ToString();
             return numeroStr[0] == '0' ? true : false;
+        }
+
+        public static bool VerificarAnoNascimento(int anoNascimento)
+        {
+            int anoAtual = DateTime.UtcNow.Year;
+            if (anoNascimento > anoAtual || anoNascimento < anoAtual - 120)
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
